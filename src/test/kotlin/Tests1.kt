918c0d003fe6931a4e09c0d704a6801c7940d4b7
Senchick <senchickpro@gmail.com>
2022-06-03 21:22:11 +0300

    init

---
+++ src/test/kotlin/Tests1.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Tests1 {
+    @Test
+    fun test1() {
+        assertEquals(2 + 3, 5)
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(2 + 2, 5)
+    }
+}
